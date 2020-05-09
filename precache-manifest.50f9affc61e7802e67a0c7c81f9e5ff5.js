bee63b8cfe06eef90d074dff629e423eddd6c66f
Adarsh S Pakkala <adarshpakkala1234@gmail.com>
2020-05-09 11:43:32 +0530

    Updates

---
+++ precache-manifest.50f9affc61e7802e67a0c7c81f9e5ff5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ec1811a49a5a4bc6f03934cbb43dabc",
+    "url": "/COVID-INFO/index.html"
+  },
+  {
+    "revision": "6835ad487c8547c3cef7",
+    "url": "/COVID-INFO/static/css/main.6f833566.chunk.css"
+  },
+  {
+    "revision": "c948ec283e1442b5c9c6",
+    "url": "/COVID-INFO/static/js/2.3810b3e7.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/COVID-INFO/static/js/2.3810b3e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6835ad487c8547c3cef7",
+    "url": "/COVID-INFO/static/js/main.5e37e7f1.chunk.js"
+  },
+  {
+    "revision": "44ad71e130f3658fde7b",
+    "url": "/COVID-INFO/static/js/runtime-main.965be502.js"
+  }
+]);
